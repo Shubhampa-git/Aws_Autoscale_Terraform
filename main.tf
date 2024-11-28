56c4dc9428502158fbd5901d087d18b5fa1dbee2
Shubham Pandey <Shubhampandey985@gmail.com>
2024-11-28 15:26:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 module "vpc" {
-  source      = "../modules/vpc"
+  source      = "./modules/vpc" # Relative path from main.tf
   cidr_block  = "10.0.0.0/16"
   name        = "my-vpc"
 }
 
 module "subnet" {
-  source            = "../modules/subnet"
+  source            = "./modules/subnet"
   vpc_id            = module.vpc.vpc_id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"  # Update to a valid AZ for your region
+  availability_zone = "ap-south-1a"
   name              = "my-subnet"
 }
 
 module "autoscaling" {
-  source        = "../modules/autoscaling"
-  ami           = "ami-0dee22c13ea7a9a67"  # Replace with the correct AMI ID you found
-  instance_type = "t2.micro"               # Replace with your desired instance type
+  source        = "./modules/autoscaling"
+  ami           = "ami-0dee22c13ea7a9a67"
+  instance_type = "t2.micro"
   subnets       = [module.subnet.subnet_id]
   name          = "my-autoscaling-group"
   min_size      = 1
